758d80805585ab1bd4f450e0d2abf6c97ca96f0e
CiroLeg <67976037+CiroLeg@users.noreply.github.com>
2021-11-05 20:30:53 -0300

    Add files via upload

---
+++ prenderMotores.ino
@@ -0,0 +1,14 @@
+// Prender motores
+
+void setup(){
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+}
+
+void loop(){
+  
+}
